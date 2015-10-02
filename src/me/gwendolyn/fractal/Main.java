43e06bd5156530f3d98897046f00b52a817b8f8d
grb23 <grb23@zips.uakron.edu>
2015-10-02 18:46:53 -0400

    Initial Commit

---
+++ src/me/gwendolyn/fractal/Main.java
@@ -0,0 +1,14 @@
+package me.gwendolyn.fractal;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setSize(800,600);
+        f.setTitle("Empty Frame");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+}
---
+++ src/me/gwendolyn/fractal/PredatorHeatVision.java
@@ -0,0 +1,12 @@
+package me.gwendolyn.fractal;
+
+import java.awt.*;
+
+/**
+ * Created by Gwendolyn on 10/2/2015.
+ */
+public class PredatorHeatVision {
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+    }
+}
